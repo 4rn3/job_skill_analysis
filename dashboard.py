9b8fbbeaba17a8d7ccda48bf22953b15d744d21a
arne <arne.lescrauwaet@hotmail.com>
2022-07-04 21:26:37 +0200

    add basic css

--- dashboard.py
+++ dashboard.py
@@ -2,64 +2,105 @@ import dash
 
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go
 
 from dash import dcc, html
 from dash.dependencies import Output, Input, State
 
 df = pd.read_csv('./required_skills.csv') 
 
-app = dash.Dash(__name__)
+external_stylesheets = [
+    'https://codepen.io/chriddyp/pen/bWLwgP.css',
+    {
+        'href': 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+        'rel': 'stylesheet',
+        'integrity': 'sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO',
+        'crossorigin': 'anonymous'
+    }
+]
+
+app = dash.Dash(__name__,external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
 
     html.Div([
         html.H1("Job skill analyzer")
-    ]),
+    ], className="banner"),
 
     html.Div([
-    html.Label("Degrees"),
-      dcc.Dropdown(
-        id="degree_dropdown",
-        options= [
-            {"label":"Master","value":"Master"},
-            {"label":"Bachelor","value":"Bachelor"},
-            {"label":"All degrees","value":"both"}
-        ],
-        value="both",
-        clearable=False
-      ),
-      dcc.Graph(
-            id="degree_graph",
-        ),
-    ]),
 
-    html.Div([
-        html.Label("Programming languages"),
-        dcc.Checklist(
-            id="programming_checklist",
-            options= ["SQL","Python","R","Java"],
-            value=["SQL","Python","R","Java"],
-            inline=True
-        ),
+        html.Div([
+        
+        html.Div([
+        html.Label("Degrees"),
+        dcc.Dropdown(
+            id="degree_dropdown",
+            options= [
+                {"label":"Master","value":"Master"},
+                {"label":"Bachelor","value":"Bachelor"},
+                {"label":"All degrees","value":"both"}
+            ],
+            value="both",
+            clearable=False
+        )
+        ], className="drop"),
         dcc.Graph(
-            id="programming_graph"
-        ),
-    ]),
+                id="degree_graph",
+            ),
+        ], className="six columns"),
 
-    html.Div([
-        html.Label("Visualisation techniques"),
-        dcc.Checklist(
-            id="visualisation_checklist",
-            options= ["Power Bi","Tableau","Dash"],
-            value=["Power Bi","Tableau","Dash"],
-            inline=True
-        ),
-        dcc.Graph(
-            id="visualisation_graph"
-        ),
-    ]),
+        html.Div([
+            html.Label("Programming languages"),
+            dcc.Checklist(
+                id="programming_checklist",
+                options= ["SQL","Python","R","Java"],
+                value=["SQL","Python","R","Java"],
+                inline=True
+            ),
+            dcc.Graph(
+                id="programming_graph"
+            ),
+        ], className="six columns"),
+
+        html.Div([
+            html.Label("Visualisation techniques"),
+            dcc.Checklist(
+                id="visualisation_checklist",
+                options= ["Power Bi","Tableau","Dash"],
+                value=["Power Bi","Tableau","Dash"],
+                inline=True
+            ),
+            dcc.Graph(
+                id="visualisation_graph"
+            ),
+        ], className="five columns"),
+
+         html.Div([
+            html.Label("Cloud"),
+            dcc.Checklist(
+                id="cloud_checklist",
+                options= ["Amazon","Google","Azure"],
+                value=["Amazon","Google","Azure"],
+                inline=True
+            ),
+            dcc.Graph(
+                id="cloud_graph"
+            ),
+        ], className="five columns"),
+
+    ], className="container"),
 ])
+
+@app.callback(
+    Output(component_id="cloud_graph",component_property="figure"),
+    [Input(component_id="cloud_checklist",component_property="value")]
+)
+def cloud_checklist(checklist_values):
+    dff = df
+
+    dff["Google"] = dff["Google"] + dff["GCP"]
+    dff["Amazon"] = dff["Amazon"] + dff["AWS"]
+
+    return px.bar(dff, x="Title", y=checklist_values, barmode="group")
 
 @app.callback(
     Output(component_id="visualisation_graph",component_property="figure"),
@@ -68,7 +109,7 @@ app.layout = html.Div([
 def visualisation_checklist(checklist_values):
 
     if "Power Bi" in checklist_values:
-        checklist_values = checklist_values.replace("Power Bi", "Power")
+        checklist_values = [item.replace("Power Bi", "Power") for item in checklist_values]
 
     dff = df
     return px.bar(dff, x="Title", y=checklist_values, barmode="group")
